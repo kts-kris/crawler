08036ca5627cee7dd3d3700c8ae5f448d62b4187
刘维刚 <liuweigang@wesai.com>
2018-01-15 17:08:53 +0800

    add worker

--- worker/Applications/Api/Article/index.php
+++ worker/Applications/Api/Article/index.php
@@ -81,7 +81,7 @@ $app->group('/article', function () {
                 $typeStr = 'article_publish_time desc';
         }
 
-        $article = Capsule::select("select articles.article_title, articles.article_brief, articles.article_images, articles.article_author, FROM_UNIXTIME(articles.article_publish_time, '%Y-%m-%d') as article_publish_time, articles.wx_title_cn, articles.wx_id, articles.weixin_tmp_url from articles where article_title <> '' order by ? limit ?,?", array($typeStr, $page, $pageSize));
+        $article = Capsule::select("select articles.article_title, articles.article_brief, articles.article_thumbnail, articles.article_author, FROM_UNIXTIME(articles.article_publish_time, '%Y-%m-%d') as article_publish_time, articles.wx_title_cn, articles.wx_id, articles.weixin_avatar from articles where article_title <> '' order by ? limit ?,?", array($typeStr, $page, $pageSize));
 
         $data = [
             'data'      =>  $article,
