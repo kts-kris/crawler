d1e42ef7b36b78dcffb40f616961206fa0fb8a08
刘维刚 <liuweigang@wesai.com>
2018-01-15 23:10:29 +0800

    add worker

--- worker/Applications/Api/Article/index.php
+++ worker/Applications/Api/Article/index.php
@@ -93,7 +93,7 @@ $app->group('/article', function () {
         }
         //$orderByStr = $typeStr . ' ' . $orderByStr;
         //Capsule::connection()->enableQueryLog();
-        $article = Capsule::table('articles')->selectRaw("select pid, articles.article_title, articles.article_brief, articles.article_thumbnail, articles.article_author, FROM_UNIXTIME(articles.article_publish_time, '%Y-%m-%d') as article_publish_time, articles.wx_title_cn, articles.wx_id, articles.weixin_avatar")->whereRaw("article_title IS NOT NULL")->orderby($typeStr, $orderByStr)->limit($pageSize)->get();
+        $article = Capsule::table('articles')->selectRaw("pid, articles.article_title, articles.article_brief, articles.article_thumbnail, articles.article_author, FROM_UNIXTIME(articles.article_publish_time, '%Y-%m-%d') as article_publish_time, articles.wx_title_cn, articles.wx_id, articles.weixin_avatar")->whereRaw("article_title IS NOT NULL")->orderby($typeStr, $orderByStr)->limit($pageSize)->get();
 //        $article = Capsule::select("select pid, articles.article_title, articles.article_brief, articles.article_thumbnail, articles.article_author, FROM_UNIXTIME(articles.article_publish_time, '%Y-%m-%d') as article_publish_time, articles.wx_title_cn, articles.wx_id, articles.weixin_avatar from articles where article_title IS NOT NULL order by ? limit ?,?", array($orderByStr, $page, $pageSize));
 //        $article = Articles::whereRaw("article_title IS NOT NULL")
 //                    ->orderBy($typeStr, $orderByStr)
