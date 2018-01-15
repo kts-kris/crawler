de834f37be69d13b3b04a5428cdc5f67b958d3a8
刘维刚 <liuweigang@wesai.com>
2018-01-15 14:45:34 +0800

    add worker

--- worker/vendor/composer/autoload_real.php
+++ worker/vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit5e82643e4a188fc18aacdbc471754e0e
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit5e82643e4a188fc18aacdbc471754e0e::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire5e82643e4a188fc18aacdbc471754e0e($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire5e82643e4a188fc18aacdbc471754e0e($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
