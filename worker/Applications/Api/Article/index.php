aafb124183f2d65becb80974238818b5ba568917
刘维刚 <liuweigang@wesai.com>
2018-01-15 18:03:35 +0800

    add worker

--- worker/Applications/Api/Article/index.php
+++ worker/Applications/Api/Article/index.php
@@ -91,8 +91,8 @@ $app->group('/article', function () {
                 $typeStr = 'article_publish_time';
         }
         //Capsule::connection()->enableQueryLog();
-        $article = Capsule::table('articles')->select(['pid', 'articles.article_title', 'articles.article_brief', 'articles.article_thumbnail', 'articles.article_author', "FROM_UNIXTIME(articles.article_publish_time, '%Y-%m-%d') as article_publish_time", 'articles.wx_title_cn', 'articles.wx_id', 'articles.weixin_avatar'])->where("article_title", '<>', "")->orderby($typeStr, 'desc')->limit($pageSize)->get();
-//        $article = Capsule::select("select pid, articles.article_title, articles.article_brief, articles.article_thumbnail, articles.article_author, FROM_UNIXTIME(articles.article_publish_time, '%Y-%m-%d') as article_publish_time, articles.wx_title_cn, articles.wx_id, articles.weixin_avatar from articles where article_title <> '' order by ? limit ?,?", array($typeStr, $page, $pageSize));
+//        $article = Capsule::table('articles')->select(['pid', 'articles.article_title', 'articles.article_brief', 'articles.article_thumbnail', 'articles.article_author', "FROM_UNIXTIME(articles.article_publish_time, '%Y-%m-%d') as article_publish_time", 'articles.wx_title_cn', 'articles.wx_id', 'articles.weixin_avatar'])->where("article_title", '<>', "")->orderby($typeStr, 'desc')->limit($pageSize)->get();
+        $article = Capsule::select("select pid, articles.article_title, articles.article_brief, articles.article_thumbnail, articles.article_author, FROM_UNIXTIME(articles.article_publish_time, '%Y-%m-%d') as article_publish_time, articles.wx_title_cn, articles.wx_id, articles.weixin_avatar from articles where article_title <> '' order by ? limit ?,?", array($typeStr, $page, $pageSize));
         //$res = Capsule::connection()->getQueryLog();
         //var_dump($res);
         $data = [
